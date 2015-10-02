2a7bd957ce40b4113f9f492e8520127c4bdc6f02
Arun <>
2015-10-02 16:56:56 -0700

    Test Commit

---
+++ src/MiscalleneousAlgos/MatrixPrint.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MiscalleneousAlgos;
+
+/**
+ *
+ * @author aramalingam
+ */
+public class MatrixPrint {
+    
+    public static void main(String[] args) {
+        int[][] matrix = 
+        {
+            {5,4,3,2,1},
+            {6,7,8,3,2},
+            {1,0,4,5,6},
+            {4,5,2,7,6},
+            {3,6,4,2,1}
+        };
+    }
+    
+}
